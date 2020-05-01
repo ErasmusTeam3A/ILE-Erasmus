e0e09689f1c3faf6a844f6d6af83cd199c0a3f00
Hayen Phan <hayenphan@outlook.com>
2020-05-01 16:35:47 +0200

    Added react router

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './../App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// Components
+import Start from '../pages/Start';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                  <Route exact path="/" component={Start} />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default App;
