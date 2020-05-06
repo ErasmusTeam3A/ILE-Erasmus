65511ce0f2107f271564b5b272bf6807d38aed3d
Mio Spruit <mio12@Hotmail.nl>
2020-05-06 13:51:39 +0200

    gitflow-feature-stash: webGL
    Add first code for webGL view of 3d object.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,14 @@ import {
 
 // Components
 import Start from '../pages/Start';
+import Model from './organisms/Model'
 
 class App extends React.Component {
     render() {
         return (
             <Router>
                 <Switch>
-                  <Route exact path="/" component={Start} />
+                  <Route exact path="/" component={Start,Model} />
                 </Switch>
             </Router>
         );
