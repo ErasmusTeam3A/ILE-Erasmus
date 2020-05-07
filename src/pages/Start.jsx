37b218c12192cd18d3c417308e0a9a02c7777688
Hayen Phan <hayenphan@outlook.com>
2020-05-07 13:03:17 +0200

    Created button component

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Button from '../components/atoms/Button';
 
 const Start = () => {
     return(
-        <div>
-            Dit is de startpagina
-        </div>
+      <div className="container">
+          <div className="start">
+              <div className="start__button-container">
+                  <div className="start__button-wrapper">
+                      <Button text="Import dicom" />
+                  </div>
+                  <div className="start__button-wrapper">
+                      <Button text="Selecteer lichaams onderdeel" />
+                  </div>
+                  <div className="start__button-wrapper">
+                      <Button text="Recent geopend" />
+                  </div>
+                  <div className="start__button-wrapper">
+                      <Button text="Afsluiten" />
+                  </div>
+              </div>
+          </div>
+      </div>
     )
 };
 
