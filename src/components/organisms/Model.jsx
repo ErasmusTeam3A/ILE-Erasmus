828efd7baa1201270ac692ef608a2a72268a268f
Hayen Phan <hayenphan@MacBook-Pro-van-Hayen.local>
2020-05-13 16:33:27 +0200

    Deleted obj files

--- src/components/organisms/Model.jsx
+++ src/components/organisms/Model.jsx
@@ -14,7 +14,7 @@ class Model extends React.Component {
     const width = this.mount.clientWidth;
     const height = this.mount.clientHeight;
     scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1000);
     this.camera.position.z = 8;
     this.camera.position.y = 5;
 
