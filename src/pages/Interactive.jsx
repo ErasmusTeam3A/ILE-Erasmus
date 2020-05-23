b44131a2d4b2250d261707b0f3c7d68a6ffbe864
Mio Spruit <mio12@hotmail.nl>
2020-05-23 16:36:26 +0200

    Work in progress menu.
    Add menu with accordion.

--- src/pages/Interactive.jsx
+++ src/pages/Interactive.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import Model from '../components/organisms/Model';
 import Back from '../components/atoms/Back';
-
+import Collapsible from 'react-collapsible';
+import { Link } from 'react-router-dom';
 
 const Interactive = () => {
     return (
         <div className="container">
             <div className="interactive">
-                <Back />
-                <Model />
+                <div className="interactive__menu">
+                    <div className="interactive__menu-heading">
+                        <h2>Menu</h2>
+                    </div>
+                    <Collapsible transitionTime="300" classParentString="collaps" trigger="Onder Lichaam">
+                        <Collapsible transitionTime="300" classParentString="collaps" trigger="Bekken">
+                            <ul>
+                                <li>Bekkenbodem</li>
+                                <li>Ligamenten baarmoeder</li>
+                                <li>Uturus</li>
+                            </ul>
+                        </Collapsible>
+                        <Collapsible transitionTime="300" classParentString="collaps" trigger="Bovenbenen">
+                            <ul>
+                                <li>Ligamenten baarmoeder</li>
+                            </ul>
+                        </Collapsible>
+                        <Collapsible transitionTime="300" classParentString="collaps" trigger="Voeten">
+                            <ul>
+                                <li>Uturus</li>
+                            </ul>
+                        </Collapsible>
+
+                    </Collapsible>
+                </div>
+
+                {/* <Back /> */}
+                {/* <Model /> */}
             </div>
         </div>
     )
