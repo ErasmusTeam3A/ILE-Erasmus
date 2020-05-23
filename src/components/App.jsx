1effe1a5b00cde723b3a6f9486c5e3cf2193a817
Mio Spruit <mio12@hotmail.nl>
2020-05-23 13:02:18 +0200

    Start page styling.
    Add home page styling and position for all elements.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,18 +8,20 @@ import {
 
 // Components
 import Start from '../pages/Start';
-import Model from './organisms/Model'
+import Interactive from '../pages/Interactive';
+// import Model from './organisms/Model'
 
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                  <Route exact path="/" component={Start,Model} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Start} />
+          <Route exact path="/model" component={Interactive} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
