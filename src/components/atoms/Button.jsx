fa8875efcaa3fe9f4a9c02b5c7be411f54e2fb8a
Mio Spruit <mio12@hotmail.nl>
2020-05-23 13:02:18 +0200

    Start page styling.
    Add home page styling and position for all elements.

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Button = (props) => {
-    return(
-        <button className="button">
-            { props.text }
-        </button>
+    return (
+        <Link to={props.link}>
+            <button className="button">
+                {props.text}
+            </button>
+        </Link>
     )
 };
 
