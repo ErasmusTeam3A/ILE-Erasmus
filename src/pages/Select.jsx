fa228ef5bcc6e9f07a08a2136a89b0c8b31f311a
lars <larsvboven@hotmail.com>
2020-05-30 11:32:03 +0200

    Title, text & back button added
    added the title, text under the title and the back button and also styled it

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import undo from '../../dist/icons/undo.png'
+
+
 const Select = () => {
     return (
         <div className="container">
             <div className="select">
                 <div className="select__content-wrapper">
                     <div className="select__head-wrapper">
-                        <h2>Selecteer lichaamsdeel</h2>
+
+                        <div className="title">
+                            <h1>Selecteer lichaamsdeel</h1>
+                        </div>
+
+                        <div className="back">
+                            <img src={undo} alt='undo'></img>
+                        </div>
+
+                        <div className="text">
+                            <p>Klik op het lichaam om <br /> een interactieve weergave te openen van dit onderdeel</p>
+                        </div>
                     </div>
                 </div>
             </div>
