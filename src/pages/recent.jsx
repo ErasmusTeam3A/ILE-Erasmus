9e34d14affda15c8396913aff64b4c97548e45f1
lars <larsvboven@hotmail.com>
2020-05-31 16:27:14 +0200

    added style and react file
    added the file for styling and content

---
+++ src/pages/recent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Start = () => {
+    return (
+        <div className="container">
+            <div className="recent">
+                <div className="recent__content-wrapper">
+                    <div className="recent__head-wrapper">
+                        <h1>
+                            I.L.G.
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
